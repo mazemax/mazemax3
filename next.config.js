73f2a50e55aed4b34865fe400a30f246a3c34376
M.Saad Siddiqui <to.msaads@gmail.com>
2023-07-22 02:38:25 +0800

    Package upgrade

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-    output: 'export',
     basePath: '/mazemax3',
+    output: 'export',
+    compress: true,
+    poweredByHeader: false,
+    devIndicators: {
+        buildActivityPosition: 'bottom-right'
+    },
+    env: {
+        customKey: 'my-value'
+    },
+    serverRuntimeConfig: {
+        // Will only be available on the server side
+        mySecret: 'secret'
+    },
+    publicRuntimeConfig: {
+        // Will be available on both server and client
+        publicFolder: '/public'
+    }
 }
 
 module.exports = nextConfig
